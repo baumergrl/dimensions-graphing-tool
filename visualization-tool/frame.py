04064d1ebec7fb7717e29159a6c2825a8807bfc2
Kristen Bean <baumergrl@gmail.com>
2018-07-11 18:29:13 -0400

    Adding more base files.

---
+++ visualization-tool/frame.py
@@ -0,0 +1,17 @@
+
+
+class Frame(object):
+    def __init__ (self, xvec, yvec, zvec, xlab, ylab, zlab, xmax, xmin, ymax, ymin, zmax, zmin):
+        self.xvec = xvec
+        self.yvec = yvec
+        self.zvec = zvec
+        self.xlab = xlab
+        self.ylab = ylab
+        self.zlab = zlab
+        self.xmax = xmax
+        self.xmin = xmin
+        self.ymax = ymax
+        self.ymin = ymin
+        self.zmax = zmax
+        self.zmin = zmin
+
